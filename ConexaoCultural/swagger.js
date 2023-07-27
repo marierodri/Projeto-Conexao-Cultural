49fa9b958a74311c62b54edf74756f113ad8364e
Marie <dudasrr24@gmail.com>
2023-07-27 14:25:39 -0300

    config Swagger

--- ConexaoCultural/swagger.js
+++ ConexaoCultural/swagger.js
@@ -1,4 +1,34 @@
 const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger/swagger_output.json';
 const endpointsFiles = ['./src/app.js'];
-swaggerAutogen(outputFile, endpointsFiles);
+
+const securityDefinitions = {
+    Bearer: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: 'Enter a valid token to test the requests below.'
+    }
+};
+
+const config = {
+    info: {
+      version: '1.0.0',
+      title: 'Conexão Cultural',
+      description:
+        'A API Conexão Cultural é uma plataforma para conectar jovens a atividades culturais. Permite o registro de usuários, pesquisa e inscrição em atividades e eventos, além do gerenciamento eficiente desses eventos por administradores.',
+    },
+    host: 'conexao-cultural.onrender.com',
+    basePath: '/',
+    schemes: ['https', 'http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    security: [
+      {
+      "Bearer": []
+      }
+    ], 
+};
+
+
+swaggerAutogen(outputFile, endpointsFiles, {...config, securityDefinitions});
